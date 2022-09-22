e54f5f1d9bb873922b183d24906aeb4900f1319d
ninam <ninamichalski@gmx.net>
2022-09-22 18:18:02 +0200

    Added filter to search for characters

--- src/Component/CharacterCard.tsx
+++ src/Component/CharacterCard.tsx
@@ -1,17 +1,19 @@
 import {Character} from "../Model/Character";
 import './CharacterCard.css';
 
-export default function CharacterCard(character : Character) {
+type CardProps = {character:Character}
+
+export default function CharacterCard(props: CardProps) {
     return(
         <div className ={"character-card"}>
             <h3>
-                {character.name}
+                {props.character.name}
             </h3>
              <p>
-                 <img src={character.image} alt={character.name}/>
+                 <img src={props.character.image} alt={props.character.name}/>
             </p>
             <p>
-                 Status : {character.status}
+                 Status : {props.character.status}
             </p>
         </div>
 
--- src/Component/CharacterGallery.tsx
+++ src/Component/CharacterGallery.tsx
@@ -1,14 +1,29 @@
 import {Character} from "../Model/Character";
 import'./CharacterGallery.css';
 import CharacterCard from "./CharacterCard";
+import React, {useState} from "react";
 
-export default function CharacterGallery(props:{characters:Character[]}) {
+type GalleryProps = {characters:Character[]}
 
-            return <div className ={"cards"}>
-                {props.characters.map((char) =>
+export default function CharacterGallery(props:GalleryProps) {
+
+    const [filterText, setFilterText] = useState("");
+
+    const result = props.characters.filter((char) => char.name.toLowerCase().includes(filterText.toLowerCase()));
+
+            return <div>
+
+                <div className={"input"}>
+                <input onChange={(event) =>
+                    setFilterText(event.target.value)}/>
+                </div>
+
+                <div className ={"cards"}>
+                {result.map((char) =>
                         <div className={"card"}>
-                {CharacterCard(char)}
+                              <CharacterCard character= {char}/>
                         </div>)}
                     </div>
 
+            </div>
 }
